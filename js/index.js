fefbe4278cfc52d66eedfe13fd51a85cef4406d2
Joshua <joshuanweze270@gmail.com>
2022-08-08 17:55:37 +0100

    I added a translator function to translate between Arabic and English, plus I edited the nav bar moving nav items to the right

---
+++ js/index.js
@@ -0,0 +1,7 @@
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({
+        // defaultLanguage: 'en',
+        pageLanguage: 'en',
+        includedLanguages: 'en,ar',
+    }, 'google_translate')
+}
